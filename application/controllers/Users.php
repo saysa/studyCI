2b588464ea7322778c249b95ead6e2a5b932ef7b
Saysa <saysabou@gmail.com>
2013-09-26 14:56:06 +0200

    extending the Core Controller with MY prefix

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -1,44 +1,7 @@
 <?php
-class Users extends CI_Controller {
+class Users extends MY_Controller {
 	
 	public $user;
-	
-	public static function redirect($url)
-	{
-		header("Location: {$url}");
-		exit();
-	}
-	
-	protected function _isSecure()
-	{
-		// get user session
-		$this->_getUser();
-		
-		if (!$this->user)
-		{
-			self::redirect("/login");
-		}
-	}
-	
-	protected function _getUser()
-	{
-		// load session library
-		$this->load->library("session");
-		
-		// get user id
-		$id = $this->session->userdata("user");
-		
-		if ($id)
-		{
-			// load user model
-			$this->load->model("user");
-			
-			// get user
-			$this->user = new User(array(
-				"id" => $id
-			));
-		}
-	}
 	
 	public function _upload($name, $user)
 	{
